8b3be569e1177381e3f6fd3d590faa1507b2f7b2
Alejandro Ibarra <coodingalex@gmail.com>
2021-08-22 22:32:35 -0600

    Update 22/08/2021

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-4c5931ee00a960d8b62f.js"
+    "url": "webpack-runtime-992560031bd960b86bb7.js"
   },
   {
     "url": "framework-6c3ead24af170cdcb87c.js"
@@ -37,7 +37,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "43b470efd892b32ad09388d7d6d3efd9"
+    "revision": "a5ee34c086cbb6c0d77de7a2630056b2"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-260e89c911c4cfae04b1.js"
